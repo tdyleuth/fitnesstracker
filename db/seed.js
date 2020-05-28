9fbc5830b2285f062573ef728cf9cca665e364e5
tdyleuth <tony.dyleuth@gmail.com>
2020-05-27 18:32:30 -0700

    Added Create & Drop table functions

--- db/seed.js
+++ db/seed.js
@@ -1,14 +1,18 @@
 const { 
     client,
     getAllUsers,
+    createUser,
  } = require('./index');
 
 
 async function dropTables() {
     try {
-        await client.query(
-            `DROP TABLE IF EXISTS users;`
-        );
+        await client.query(`
+            DROP TABLE IF EXISTS routine_activities;
+            DROP TABLE IF EXISTS routines;
+            DROP TABLE IF EXISTS activities;
+            DROP TABLE IF EXISTS users;
+            `);
   
         console.log("Finished dropping tables!");
   
@@ -27,7 +31,33 @@ async function dropTables() {
               id SERIAL PRIMARY KEY,
               username VARCHAR(255) UNIQUE NOT NULL,
               password VARCHAR(255) NOT NULL
-             )`
+             );`
+         );
+
+         await client.query(
+             `CREATE TABLE activities (
+              id SERIAL PRIMARY KEY,
+              name VARCHAR(255) UNIQUE NOT NULL,
+              description TEXT NOT NULL
+             );`
+         );
+
+         await client.query(
+             `CREATE TABLE routines (
+              id SERIAL PRIMARY KEY,
+              "creatorId" INTEGER REFERENCES users(id) NOT NULL,
+              public BOOLEAN DEFAULT false,
+              goal TEXT NOT NULL
+             );`
+         );
+
+         await client.query(
+             `CREATE TABLE routine_activities (
+              id SERIAL PRIMARY KEY,
+              "routineId" INTEGER REFERENCES routines(id) NOT NULL,
+              "activityId" INTEGER REFERENCES activities(id),
+              count INTEGER NOT NULL
+             );`
          );
   
       console.log("Finished creating tables!");
