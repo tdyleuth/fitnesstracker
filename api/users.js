bc56e062b7837f7cea5d33c505c12f14015c564a
tdyleuth <tony.dyleuth@gmail.com>
2020-06-03 18:50:51 -0700

    Update Routes

--- api/users.js
+++ api/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const usersRouter = express.Router();
 const jwt = require('jsonwebtoken');
 
-const  { getAllUsers, getUserByUsername } = require('../db')
+const  { getAllUsers, getUserByUsername, createUser } = require('../db')
 
 usersRouter.use((req, res, next) => {
     console.log("A request is being made to /users");
@@ -50,5 +50,41 @@ usersRouter.post('/login', async (req, res, next) => {
       next(error);
     }
   });
+
+
+usersRouter.post('/register', async (req, res, next) => {
+  const { username, password, name } = req.body;
+
+  try {
+    const userExists = await getUserByUsername(username);
+
+    if(userExists){
+      next({
+        name: 'UserExistsError',
+        message: 'A user by that name already Exists'
+      });
+    }
+    
+     const user = await createUser({
+       username,
+       password,
+       name,
+     });
+
+     const token = jwt.sign({ 
+      id: user.id, 
+      username
+    }, process.env.JWT_SECRET, {
+      expiresIn: '1w'
+    });
+
+    res.send({ 
+      message: "Thank you for signing up!",
+      token 
+    });
+  } catch ({ name, message }) {
+    next({ name, message })
+  } 
+});
   
 module.exports = usersRouter;
